970a98cb026eafc2763b501e64b419cbd4e74c18
hubcad25 <hubertcadieux@gmail.com>
2023-10-03 17:35:17 -0400

    fix visite famille

--- cleaning_maroc_hommes.R
+++ cleaning_maroc_hommes.R
@@ -269,15 +269,15 @@ table(dataH$`Combien de fois rendez-vous visite à votre famille?`)
 cleanData$visitefamille <- NA
 
 cleanData$visitefamille[dataH$`Combien de fois rendez-vous visite à votre famille?` ==
-                          "Plusieurs fois par mois"] <- 0.75
+                          "Plusieurs fois par mois"] <- 0.25
 cleanData$visitefamille[dataH$`Combien de fois rendez-vous visite à votre famille?` ==
-                          "Plusieurs fois par semaine"] <- 0.5
+                          "Plusieurs fois par semaine"] <- 0.75
 cleanData$visitefamille[dataH$`Combien de fois rendez-vous visite à votre famille?` ==
                           "Tous les jours"] <- 1
 cleanData$visitefamille[dataH$`Combien de fois rendez-vous visite à votre famille?` ==
                           "Une fois par mois"] <- 0
 cleanData$visitefamille[dataH$`Combien de fois rendez-vous visite à votre famille?` ==
-                          "Une fois par semaine"] <- 0.25
+                          "Une fois par semaine"] <- 0.5
 
 table(cleanData$visitefamille)
 
