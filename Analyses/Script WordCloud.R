6043f0b38dcb280e2da0d0f1642c4b0f675e6df3
Arnaud Beaulé <arnaud@MacBook-Air-de-Arnaud-3.local>
2023-10-23 10:48:41 -0400

    cloudword marche

--- Analyses/Script WordCloud.R
+++ Analyses/Script WordCloud.R
@@ -7,10 +7,6 @@ library(wordcloud2)
 library(svglite)
 library(webshot)
 library(htmlwidgets)
-
-
-
-
 library(ggwordcloud)
 
 ### Load Data ####
@@ -32,24 +28,27 @@ docs <- docs %>%
   tm_map(removePunctuation) 
 
 docs <- tm_map(docs, content_transformer(tolower))
-docs <- tm_map(docs, removeWords, c(stopwords("french"), 'veuillez',"autre","grand","spécifier","autres","santé"))
+docs <- tm_map(docs, removeWords, c(stopwords("french"), 'veuillez',"autre","grand","spécifier","autres","santé", "problèmes"))
 
 dtm <- TermDocumentMatrix(docs) 
 matrix <- as.matrix(dtm) 
 words <- sort(rowSums(matrix),decreasing=TRUE) 
 df <- data.frame(word = names(words),freq=words)
 
-ggplot(df, aes(label = word, size = freq)) +
-  geom_text_wordcloud()
+ggplot(df, aes(label = word, size = freq, color = freq)) +
+  geom_text_wordcloud() +
+  scale_color_gradient(low = "darkgrey", high = "darkblue") +
+  clessnverse::theme_clean_light() +
+  scale_size_area(max_size = 18) 
 
 ggsave("graphs/cloudword/probsantecourant.png",
-       width = 47,height = 43, limitsize = FALSE)
+       width = 15 ,height = 15)
 
 
 
 #### openprobsante homme/femme -----------------------------------------------------
-text_hommes <- data$openprobsantecourant[data$ses_sex == "homme"]
-text_femmes <- data$openprobsantecourant[data$ses_sex == "femme"]
+text_hommes <- data$openprobsantecourant[data$ses_sex == "male"]
+text_femmes <- data$openprobsantecourant[data$ses_sex == "female"]
 
 # Prétraitement du texte pour les hommes
 text_hommes <- gsub("[[:punct:]]", " ", text_hommes)
@@ -58,7 +57,7 @@ docs_hommes <- docs_hommes %>%
   tm_map(removeNumbers) %>%
   tm_map(removePunctuation) %>%
   tm_map(content_transformer(tolower)) %>%
-  tm_map(removeWords, c(stopwords("french"), 'veuillez', 'autre', 'grand', 'spécifier', 'autres'))
+  tm_map(removeWords, c(stopwords("french"), 'veuillez', 'autre', 'grand', 'spécifier', 'autres', "problèmes"))
 
 # Prétraitement du texte pour les femmes
 text_femmes <- gsub("[[:punct:]]", " ", text_femmes)
@@ -67,27 +66,35 @@ docs_femmes <- docs_femmes %>%
   tm_map(removeNumbers) %>%
   tm_map(removePunctuation) %>%
   tm_map(content_transformer(tolower)) %>%
-  tm_map(removeWords, c(stopwords("french"), 'veuillez', 'autre', 'grand', 'spécifier', 'autres'))
+  tm_map(removeWords, c(stopwords("french"), 'veuillez', 'autre', 'grand', 'spécifier', 'autres','problèmes'))
 
 # Créez le nuage de mots pour les hommes
 dtm_hommes <- TermDocumentMatrix(docs_hommes)
 matrix_hommes <- as.matrix(dtm_hommes)
 words_hommes <- sort(rowSums(matrix_hommes), decreasing = TRUE)
-df_hommes <- data.frame(word = names(words_hommes), freq = words_hommes)
+df_hommes <- data.frame(word = names(words_hommes), freq = words_hommes) %>% 
+  mutate(sex = "Hommes")
 
 # Créez le nuage de mots pour les femmes
 dtm_femmes <- TermDocumentMatrix(docs_femmes)
 matrix_femmes <- as.matrix(dtm_femmes)
 words_femmes <- sort(rowSums(matrix_femmes), decreasing = TRUE)
-df_femmes <- data.frame(word = names(words_femmes), freq = words_femmes)
+df_femmes <- data.frame(word = names(words_femmes), freq = words_femmes) %>% 
+  mutate(sex = "Femmes")
 
-# Créez le nuage de mots pour les hommes
-wordcloud2(df_hommes, backgroundColor = "white", color = "random-light")
-ggsave("hommes_wordcloud.png", width = 15, height = 13)
+df <- rbind(df_hommes, df_femmes)
 
-# Créez le nuage de mots pour les femmes
-wordcloud2(df_femmes, backgroundColor = "white", color = "random-light")
-ggsave("femmes_wordcloud.png", width = 15, height = 13)
+ggplot(df, aes(label = word, size = freq, color = freq)) +
+  geom_text_wordcloud() +
+  scale_color_gradient(low = "darkgrey", high = "darkblue") +
+  clessnverse::theme_clean_light(base_size = 15) +
+  scale_size_area(max_size = 13) +
+  facet_wrap(~sex, nrow = 2) +
+  theme(strip.text = element_text(size = 25))
+
+ggsave("graphs/cloudword/OpenprobsanteXsex.png",
+       width = 15, height = 13)
+
 
 
 
@@ -144,8 +151,6 @@ df <- data.frame(word = names(words),freq=words)
 
 wordcloud3 <- wordcloud2(df, shape = "pentagon")
 
-saveWidget(wordcloud3, file = "wordcloud3.html")
-
 ggsave("graphs/cloudword/opendanger.png",
        width = 15,height = 13)
 
@@ -172,6 +177,6 @@ df <- data.frame(word = names(words),freq=words)
 
 wordcloud2(df, shape = "diamond")
 
-saveWidget(wordcloud2()"graphs/cloudword/openamelio.html", selfcontained = F)
+
 
 
