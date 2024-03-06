62d288fa2331368acf9f3bb0a611133b03c32d6a
Arnaud Beaulé <arnaud@MacBook-Air-de-Arnaud-3.local>
2024-03-06 11:37:10 -0500

    codebook

--- cleaning/merge.R
+++ cleaning/merge.R
@@ -1,3 +1,4 @@
+library(dplyr)
 h <- readRDS("Data/hommes.rds") %>% 
   mutate(ses_sex = "male")
 f <- readRDS("Data/femmes.rds") %>% 
@@ -6,3 +7,4 @@ f <- readRDS("Data/femmes.rds") %>%
 data <- merge(h, f, all = TRUE)
 
 saveRDS(data, "Data/data.rds")
+writexl::write_xlsx(data, "Data/data.xlsx")
